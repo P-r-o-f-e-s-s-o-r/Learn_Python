3c67d63cca65dfce7e28f024e7b5cc79112ed77d
Mukeshwar Raudra <mukeshwarraudra369@gmail.com>
2026-05-31 17:33:56 +0530

    SM added

---
+++ String_Manipulation.py
@@ -0,0 +1,11 @@
+#ASK USER FOR USERNAME AND PCODE . MAKE SURE THAT PCODE DOESNT CONTAIN USERNAME AND MATCHES "Trident111"
+'''
+U_name = input("Enter the User Name : ")
+pcoode = input("Enter the PASSWORD: ")
+
+if (pcoode not in U_name and pcoode == "Trident111"):
+    print("Code is Valid")
+else:
+    print("Invalid")
+'''
+
