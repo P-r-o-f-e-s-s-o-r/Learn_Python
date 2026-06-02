79ad84544ca2682ae4d33eb45d300e4b90b0a528
Mukeshwar Raudra <mukeshwarraudra369@gmail.com>
2026-06-03 00:18:06 +0530

    F added

---
+++ Functions.py
@@ -0,0 +1,11 @@
+'''
+#PROGRAME TO ADD NUMBERS 
+
+def sum(x,y):
+    s = x+y
+    return s
+num1 = int(input("Enter the Number 1 : "))
+num2 = int(input("Enter the Number 2 : "))
+k = sum(num1,num2)
+print("The Sum of",num1,"and",num2,"is : ",k)
+'''
