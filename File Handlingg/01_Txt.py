1f51f82a017f293f554f5279f3c4675108ac0c65
Mukeshwar Raudra <mukeshwarraudra369@gmail.com>
2026-06-03 14:55:44 +0530

    FH_T updated

--- File Handlingg/01_Txt.py
+++ File Handlingg/01_Txt.py
@@ -1,3 +1,4 @@
+'''
 file = open("poem.txt","r")
 red = file.read(10)   #READ ONLY 10 BYTES
 print(red,end="")
@@ -5,8 +6,19 @@ red1 = file.read(30) #READ OTHER SET OF LINES
 print(red1)
 
 '''
+
+'''
 re = file.read()
 print(re)
 for line in file:
    print(line)
 '''
+
+#write
+file = open("Names.txt","w")
+n = int(input("Number of people : "))
+for i in range(n):
+    name = input("Enter The Name : ")
+    file.write(name)
+    file.write("\n")
+file.close()
