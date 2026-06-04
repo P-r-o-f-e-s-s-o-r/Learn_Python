1a5e09e2086ce9286e077ae0a8467bcae3882a80
Mukeshwar Raudra <mukeshwarraudra369@gmail.com>
2026-06-04 22:41:29 +0530

    01_Numpy updated

--- Numpy/01_Numpy.py
+++ Numpy/01_Numpy.py
@@ -1,7 +1,30 @@
-import numpy as np
+import numpy as np  #Import Numpy
 
 array = [12,4,2]
-array = np.array(array)
-
+array = np.array(array) #Converting Normal list to Numpy array
+'''
 print(array)
-print(array*2)
+array = array*2 
+print(array)
+
+'''
+'''
+#DTYPE 
+print(array.dtype)
+arra = np.array([1,2,3],dtype = np.int8)
+print(arra.dtype)
+print(arra.nbytes) #No of Elements [Result = 3 (3*1)]
+'''
+#int8, int16, int32, int64 []
+#2^8=256 >-128 --> 127 [If it exceed 127 means Error will be occur]
+
+'''
+#BYTES
+print(array.dtype)
+arra = np.array([1,2,3],dtype = np.int64)
+print(arra.dtype)
+print(arra.nbytes) #No of Elements [Result = 24 (3*8)] [no of elements * (bytes/8)]
+
+arra = np.array([1,2,3],dtype = np.str_) #STRING
+print(arra)
+'''
