19d6bfb14f8f0cbeb46cf1bf3015dd7996ccb395
Mukeshwar Raudra <mukeshwarraudra369@gmail.com>
2026-06-15 16:58:57 +0530

    02_Numpy Updated

--- Numpy/02_Numpy.py
+++ Numpy/02_Numpy.py
@@ -85,3 +85,23 @@ brr =np.array([[1,2,3],
 print(np.sum(brr))
 print(np.sum(brr,axis=1))
 '''
+'''
+marks = np.array([50,100,65,45,76])
+print(marks)
+print(marks[marks>50]) #Tells the value
+print(marks>50) #Tells the Boolean Value
+
+#Filtered Array
+pass_mark = marks[marks>50]
+print(pass_mark)
+
+even = marks[(marks>=50) | (marks%2==0)]
+print(even)
+
+arr = np.array([[1,2,3],
+                [4,5,6]])
+
+even = np.where(arr%2==0, arr, 0) #Except Even Number all places is filled with Zero . OUTPUT will be of Same Dimentsons
+print(even)
+
+'''
