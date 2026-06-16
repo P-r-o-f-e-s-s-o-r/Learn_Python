1927d1ec4438479db18064025f623fdfbdebb012
Mukeshwar Raudra <mukeshwarraudra369@gmail.com>
2026-06-16 15:26:53 +0530

    01_Pandas Updated

--- AIML/01_Pandas.py
+++ AIML/01_Pandas.py
@@ -1,2 +1,40 @@
 import pandas as pd
-print(pd.__version__)
+
+#SERIES 
+#/////////////////////////////////////////////
+#age = [23,42,1,31]
+'''
+series = pd.Series(age)
+print(series)
+'''
+#Using Index to Access the Element 
+'''
+print(series.loc[1])
+print(age[1])
+'''
+#Manual Assigning Index Value 
+'''
+series = pd.Series(age, index = ["A","B","C","D"])
+print(series.loc["A"])
+print(series)
+print(age[1])
+'''
+
+#Convert Dictionary to Series 
+age = {
+    "Mukesh": 18,
+    "Taazim": 19,
+    "giri": 19
+}
+series = pd.Series(age)
+print(series)
+print(series[series>18])
+#/////////////////////////////////////////////
+
+#DATAFRAME
+
+students = {
+    "Name" : ["Goms","Siva","Code io"],
+    "Age" : [25,22,5],
+    "CGPA" : [8.5,10,5]
+}
