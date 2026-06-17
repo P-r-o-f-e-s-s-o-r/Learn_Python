1b1f552d41aa2f1ec5cfc9b55fdb45370067fb88
Mukeshwar Raudra <mukeshwarraudra369@gmail.com>
2026-06-17 14:19:02 +0530

    02_Pandas Updated

--- AIML/02_Pandas.py
+++ AIML/02_Pandas.py
@@ -1,4 +1,17 @@
 import pandas as pd
-df = pd.read_csv("customers-100.csv")
+#df = pd.read_csv("customers-100.csv")
 #print(df)
-print(df["City"].to_string()) # Print Every City
+#print(df["City"].to_string()) # Print Every City
+#print(df[["Customer Id","First Name","City"]].to_string()) # Print Every Columns
+'''
+df = pd.read_csv("customers-100.csv",index_col = "Customer Id") # Make the indexs as CID
+#print(df)
+print(df.loc["CeD220bdAaCfaDf",["First Name","Last Name"]]) #Particular columns
+'''
+
+df = pd.read_json("iris.json")
+print(df)
+df = df.drop(columns = ["status"]) #DELETE A COLUMN
+print(df)
+df = df.dropna(subset = ["first_name"]) #Whereever NaN is present it will delete that
+print(df)
