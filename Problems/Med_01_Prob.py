60b76c9af825160382bc66bde5f7850fe0e11a99
Mukeshwar Raudra <mukeshwarraudra369@gmail.com>
2026-06-18 22:54:18 +0530

    Med_Prob Updated

--- Problems/Med_01_Prob.py
+++ Problems/Med_01_Prob.py
@@ -1,2 +1,13 @@
 #STUDENTS PERFORMANCE ANLYZER
 #Build a program that analyzes marks of students and generates a report.
+
+import numpy as np
+def main():
+    while True:
+        print("*"*5,"STUDENTS PERFORMANCE ANALYZER","*"*5)
+        print(" "*5,"1. ADD STU DETAILS")
+        print(" "*5,"2. CALCULATE AVG MARK")
+        print(" "*5,"3. FIND GRADE")
+        print(" "*5,"4. GENERATE REPORT")
+        
+
